5ef07b801c00dbe2a14afa1f847e3040135c7c56
mateus lucas <108986368+Mateuslucs@users.noreply.github.com>
2023-01-30 07:40:08 -0300

    testes

---
+++ dhio.php
@@ -0,0 +1,10 @@
+<?php
+$nome = "Dhiovana é linda";
+$lista_irmao = ["marcos", "sulo", "bio", "nina"];
+//echo $nome;
+
+//echo $lista_irmao[3];
+foreach($lista_irmao as $irmao){
+    echo $irmao."\n";
+}
+?>
