fb3022f6dd24cacffa8330f559dbfcacf9e74b9a
mateus lucas <108986368+Mateuslucs@users.noreply.github.com>
2023-04-10 20:02:29 -0300

    private,public and protected

---
+++ poo/aula02/caneta.php
@@ -0,0 +1,26 @@
+<?php
+class caneta{
+    public $modelo;
+    public $cor;
+    private $ponta;
+    protected $carga;
+    protected $tampada;
+
+    public function rabiscar(){
+        if($this->tampada){
+            echo "<p>ERRO! Não posso rabiscar</p>";
+        }else{
+            echo "<p>Estou rabiscando...</p>";
+        }
+        
+    }
+
+    public function tampar(){
+        $this->tampada = true;
+    }
+
+    public function destampar(){
+        $this->tampada = false;
+    }
+}
+?>
