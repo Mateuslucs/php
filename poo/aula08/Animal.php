0b6bb04b7904cd0e0f4bc207f65b48c0d5ceb1b9
mateus lucas <108986368+Mateuslucs@users.noreply.github.com>
2023-09-25 07:59:52 -0300

    polimorfismo

---
+++ poo/aula08/Animal.php
@@ -0,0 +1,19 @@
+<?php
+
+abstract class Animal{
+    protected $weight;
+    protected $year;
+    protected $limbs;
+
+    public function move(){
+        
+    }
+
+    public function feed(){
+    
+    }
+
+    public function makeSound(){
+        
+    }
+}
